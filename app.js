d1d2f0d13c6901d84124ee0c847c4ca09f16a09f
Masudthedevops <rahmanmdm16@gmail.com>
2023-01-21 23:17:57 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Indeed with hardship comes ease-Al Quran!'));
+app.get('/', (req, res) => res.send('Indeed with hardship comes ease-Quran!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
