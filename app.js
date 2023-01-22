b275a896e136bb781864381d2d26fb4191afd4e7
Masudthedevops <rahmanmdm16@gmail.com>
2023-01-21 23:07:29 -0600

    updating message to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello people!!Thank you for visiting our site. Have a good day!'));
+app.get('/', (req, res) => res.send('Indeed with hardship comes ease!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
