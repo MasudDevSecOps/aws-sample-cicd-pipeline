60af6d3d49841f8321441eaf0dc4cd013080c138
Masudur Rahman <masud@Masudurs-MBP.attlocal.net>
2023-02-02 19:01:08 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Changing this message to test the git!!'));
+app.get('/', (req, res) => res.send('Finalize the pipeline by adding a manual approval stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
